a750f6687f83591ae88767424c892323341d31ee
Raghav Pandit <panditraghav18@gmail.com>
2024-08-25 13:21:58 +0530

    Setup basic meson project for repo

---
+++ src/algorithms/sort.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+void test() { std::cout << "Sorting" << std::endl; }
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "sort.h"
+#include <iostream>
+
+using namespace std;
+int main() {
+  cout << "Hello World" << endl;
+  test();
+  return 0;
+}
